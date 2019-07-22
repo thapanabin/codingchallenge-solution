aa3bff03eec015b3b9df7eea146de1af85a7f673
thapanabin <thapanabincr7@outlook.com>
2019-07-22 13:39:37 -0500

    added codingchallenge example with solution

---
+++ src/com/nabin/arraycodingchallenge/SumOfTwoNumExistInArray.java
@@ -0,0 +1,29 @@
+package com.nabin.arraycodingchallenge;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SumOfTwoNumExistInArray {
+    public static void main(String[] args) {
+        int[] numbers = {2, 3, 7, 4, 8};
+        int target = 20;
+        int[] result = getTwoSum(numbers,target);
+        System.out.println(result[0]+" "+result[1]);
+
+    }
+
+    public static int[] getTwoSum(int[] numbers, int target) {
+        Map<Integer, Integer> visitedNumbers = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            int delta = target - numbers[i];
+            if (visitedNumbers.containsKey(delta)) {
+                return new int[]{i, visitedNumbers.get(delta)};
+            }
+            visitedNumbers.put(numbers[i], i);
+        }
+        return new int[]{-1,-1};
+    }
+
+
+
+}
